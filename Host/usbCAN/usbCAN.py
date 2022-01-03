b62523a62893591634f6db08e254b2d83a4a6b03
XiaojiaoChen <yii402072495@gmail.com>
2022-01-03 08:49:43 +0800

    add host

---
+++ Host/usbCAN/usbCAN.py
@@ -0,0 +1,233 @@
+from ctypes import *
+import threading
+import queue
+
+
+
+
+COM_OK       = 0
+COM_ERROR    = 1
+COM_ABORT    = 2
+COM_TIMEOUT  = 3
+
+class VCI_INIT_CONFIG(Structure):
+    _fields_ = [("AccCode", c_uint),
+                ("AccMask", c_uint),
+                ("Reserved", c_uint),
+                ("Filter", c_ubyte),
+                ("Timing0", c_ubyte),
+                ("Timing1", c_ubyte),
+                ("Mode", c_ubyte)
+                ]
+
+
+class VCI_CAN_OBJ(Structure):
+    _fields_ = [("ID", c_uint),
+                ("TimeStamp", c_uint),
+                ("TimeFlag", c_ubyte),
+                ("SendType", c_ubyte),
+                ("RemoteFlag", c_ubyte),
+                ("ExternFlag", c_ubyte),
+                ("DataLen", c_ubyte),
+                ("Data", c_ubyte*8),
+                ("Reserved", c_ubyte*3)
+                ]
+
+
+
+
+class USB_CAN:
+    def __init__(self, CAN_ID=0x0400, baud=1000000):
+
+        self.VCI_USBCAN2 = 4
+        self.STATUS_OK = 1
+        self.canDLL = cdll.LoadLibrary('./libcontrolcan.so')
+
+        self.CAN_ID = CAN_ID
+        self.channelStatus = [0, 0]
+
+
+        self.receiving_alive=0
+        self.keyboard_alive=0
+        self.threads=[]
+        self.kbdQueue = queue.Queue()
+
+        self.rxbuf=[]
+
+        rx_vci_can_obj_type = VCI_CAN_OBJ*2500
+        self.rx_vci_can_obj = rx_vci_can_obj_type()
+
+        self.tx_vci_can_obj = VCI_CAN_OBJ()
+
+        ret = self.canDLL.VCI_OpenDevice(self.VCI_USBCAN2, 0, 0)
+        if ret == self.STATUS_OK:
+            print('Open USB_CAN Device successful')
+
+        else:
+            print('Open USB_CAN Device failed')
+
+
+
+    def open(self, chn=0):
+        #Init and Start CAN channel
+        self.vci_initconfig = VCI_INIT_CONFIG(
+            0x80000008, 0xFFFFFFFF, 0, 0, 0x00, 0x14, 0)  # 1M baudrate, 87.5%,  normal mode, all ID acceptable
+        ret = self.canDLL.VCI_InitCAN(
+            self.VCI_USBCAN2, 0, chn, byref(self.vci_initconfig))
+        if ret != self.STATUS_OK:
+            print('Init CAN Channel {} fail'.format(chn))
+        ret = self.canDLL.VCI_StartCAN(self.VCI_USBCAN2, 0, chn)
+        if ret == self.STATUS_OK:
+            self.channelStatus[chn] = 1
+        else:
+            print('Start CAN Channel {} fail'.format(chn))
+        return ret
+
+    def setTxID(self,canID=0x0400):
+        self.CAN_ID = canID
+
+    def start_keyboard(self):
+        self.keyboard_alive = 1
+        print('Ready for keyboard input:')
+        self.keyboardThread = threading.Thread(target=self.keyboard_thread)
+        self.threads.append(self.keyboard_thread)
+        self.keyboardThread.start()
+        
+    def getInput(self):
+        return self.kbdQueue.get()
+
+    def keyboard_thread(self):
+
+        while(self.keyboard_alive==1):
+            try:
+                input_str = input()
+                if (len(input_str) > 0):
+                    self.kbdQueue.put(input_str)
+                    #customize terminal input action here
+                    self.inputHex=input_str.encode('utf-8')
+                    self.transmit(self.inputHex,len(self.inputHex))
+                    
+
+
+
+
+
+
+            except:
+                print("quit keyboard thread")
+                break
+
+
+
+   
+
+    def start_receiving(self, chn=0):
+        self.receiving_alive=1
+        print("start receiving Can info:")
+        self.receivingThread = threading.Thread(target=self.receiving_thread,args=(chn,))
+        self.threads.append(self.receivingThread)
+        self.receivingThread.start()
+
+    def receiving_thread(self, chn=0):
+        if(self.channelStatus[chn] == 1):
+            while(self.receiving_alive):
+                try:
+                    rxNB=0
+                    #Show Rx
+                    while rxNB <= 0 and self.receiving_alive:
+                        rxNB = self.canDLL.VCI_Receive(self.VCI_USBCAN2, 0, chn, byref(self.rx_vci_can_obj), 2500, 0)
+                        #print("Wait Rx")
+                        # aa+=1
+                    for i in range (rxNB):
+                        
+                        #customize receiving action here
+                        #print("CAN channel {} Receive ID:{}, Len:{}, Data: {} ".format(chn,self.rx_vci_can_obj[i].ID,self.rx_vci_can_obj[i].DataLen,list(self.rx_vci_can_obj[i].Data)))
+                        dlen=self.rx_vci_can_obj[i].DataLen
+                        print("NB={}, dalaLen={}".format(rxNB,dlen))
+                        print(bytearray(self.rx_vci_can_obj[i].Data[:dlen]).decode(), end='', flush=True)
+                except:
+                    print("quit receiving thread")
+                break
+
+        else:
+            print("Rx Channel {} Not opened".format(chn))
+
+    def transmit(self,pdata,num):
+        ret=COM_OK
+        frameNB=num//8
+        remBytesNB=num%8
+        pdataInd=0
+        for i in range(frameNB):
+            if(ret==COM_OK):
+                ret = self.transmit_Frame(pdata[pdataInd:],8)
+                pdataInd+=8
+        if(ret==COM_OK and remBytesNB!=0):
+            ret = self.transmit_Frame(pdata[pdataInd:],remBytesNB)
+        return ret
+
+    def transmit_Frame(self, frameData, dalalen, chn=0):
+        try:
+            ret = COM_OK
+            if(self.channelStatus[chn] == 1):
+                self.tx_vci_can_obj.ID=self.CAN_ID
+                self.tx_vci_can_obj.SendType=1
+                self.tx_vci_can_obj.DataLen=dalalen
+                for i in range(dalalen):
+                    self.tx_vci_can_obj.Data[i]=frameData[i]
+                if(self.canDLL.VCI_Transmit(self.VCI_USBCAN2, 0, chn, byref(self.tx_vci_can_obj), 1)!=1):
+                    ret=COM_ERROR
+            else:
+                print("Tx Error, Channel {} Not opened".format(chn))
+            return ret
+        except:
+            print("Tx Frame timeout")
+            return COM_TIMEOUT
+
+    def receive(self,pdata,num,chn=0):
+        try:
+            if(self.channelStatus[chn] == 1):
+                ret=COM_OK
+                rxNB=0
+                numRd=0
+                pdataInd=0
+                while(pdataInd<num):
+
+                        #get all buffered Rx frames
+                        while rxNB <= 0:
+                            rxNB = self.canDLL.VCI_Receive(self.VCI_USBCAN2, 0, chn, byref(self.rx_vci_can_obj), 2500, 0)
+                        
+                        #copy Rx datas into pdata
+                        for i in range(rxNB):
+                            for j in range(self.rx_vci_can_obj[i].DataLen):
+                                pdata[pdataInd]=self.rx_vci_can_obj[i].Data[j]
+                                pdataInd+=1
+                                if(pdataInd>num):
+                                    ret=COM_ERROR
+                                    return ret
+                        #reset rx num           
+                        rxNB=0
+                return ret                    
+            else:
+                return COM_ERROR
+        except TimeoutError:
+            print("Rx timeout")
+            return COM_TIMEOUT
+
+    def close(self):
+        ret = self.canDLL.VCI_CloseDevice(self.VCI_USBCAN2, 0)
+        print("CAN device closed")
+        self.receiving_alive=0
+        self.keyboard_alive=0
+        for threadOb in self.threads:
+            threadOb.join()
+        
+if __name__ == "__main__":
+    usbcan = USB_CAN()
+    usbcan.open(0)
+    usbcan.open(1)
+    try:
+        usbcan.start_receiving(1)
+        usbcan.start_keyboard()
+    except:
+        usbcan.close()
+   
---
+++ Host/ymodem.py
@@ -0,0 +1,292 @@
+from usbCAN import USB_CAN
+
+import numpy as np
+from enum import Enum
+import time
+import os
+
+canbus = USB_CAN()
+
+
+USER_FLASH_SIZE = 0x0803F800 - 0x08004000
+
+COM_OK       = 0
+COM_ERROR    = 1
+COM_ABORT    = 2
+COM_TIMEOUT  = 3
+COM_DATA     = 4
+COM_LIMIT    = 5
+
+PACKET_HEADER_SIZE=      3
+PACKET_DATA_INDEX  =     4
+PACKET_START_INDEX   =   1
+PACKET_NUMBER_INDEX  =   2
+PACKET_CNUMBER_INDEX =   3
+PACKET_TRAILER_SIZE  =   2
+PACKET_OVERHEAD_SIZE =   (PACKET_HEADER_SIZE + PACKET_TRAILER_SIZE - 1)
+PACKET_SIZE      =      128
+PACKET_1K_SIZE    =     1024
+
+# #/-------- Packet in IAP memory ------------------------------------------\
+#  * | 0      |  1    |  2     |  3   |  4      | ... | n+4     | n+5  | n+6  | 
+#  * |------------------------------------------------------------------------|
+#  * | unused | start | number | !num | data[0] | ... | data[n] | crc0 | crc1 |
+#  * \------------------------------------------------------------------------/
+#  * the first byte is left unused for memory alignment reasons 
+
+
+FILE_NAME_LENGTH=        64
+FILE_SIZE_LENGTH=        16
+SOH             =        0x01 # start of 128-byte data packet */
+STX             =        0x02 # start of 1024-byte data packet */
+EOT             =        0x04 # end of transmission */
+ACK             =        0x06 # acknowledge */
+NAK             =        0x15 # negative acknowledge */
+CA              =        0x18 #two of these in succession aborts transfer */
+CRC16           =        0x43 # 'C' == 0x43, request 16-bit CRC */
+NEGATIVE_BYTE   =        0xFF
+ABORT1          =        0x41 # 'A' == 0x41, abort by user */
+ABORT2          =        0x61 # 'a' == 0x61, abort by uNAK_TIMEOUT     =        _t)0x100000)
+DOWNLOAD_TIMEOUT=        1000 #One second retry delay */
+MAX_ERRORS      =        5
+
+PACKET_1K_SIZE + PACKET_DATA_INDEX + PACKET_TRAILER_SIZE
+
+aPacketData=bytearray(PACKET_1K_SIZE + PACKET_DATA_INDEX + PACKET_TRAILER_SIZE)
+
+#p_data is a bytearray
+#p_file_name is a string
+
+def PrepareIntialPacket(p_data, p_file_name, length):
+
+    p_data[PACKET_START_INDEX] = SOH
+    p_data[PACKET_NUMBER_INDEX] = 0x00
+    p_data[PACKET_CNUMBER_INDEX] = 0xff
+
+    #Filename writte
+    fileNameLen=len(p_file_name)
+    p_data[PACKET_DATA_INDEX:PACKET_DATA_INDEX+fileNameLen] = p_file_name.encode('utf-8')
+    p_data[PACKET_DATA_INDEX+fileNameLen] = 0x00
+
+    #file size written
+    lenInd=PACKET_DATA_INDEX+fileNameLen + 1
+    astring = str(length).encode('utf-8')
+    p_data[lenInd:lenInd+len(astring)]=astring
+
+    padInd=lenInd+len(astring)
+    for i in range(padInd,PACKET_SIZE + PACKET_DATA_INDEX):
+      p_data[i]=0x00
+
+
+def PreparePacket(p_source, p_packet,pkt_nr,size_blk):
+  #Make first three packet */
+  packet_size = PACKET_1K_SIZE if  size_blk >= PACKET_1K_SIZE else PACKET_SIZE
+  size = size_blk if size_blk < packet_size  else packet_size
+  if (packet_size == PACKET_1K_SIZE):
+    p_packet[PACKET_START_INDEX] = STX
+  else:
+    p_packet[PACKET_START_INDEX] = SOH
+  
+  p_packet[PACKET_NUMBER_INDEX] = int.to_bytes(pkt_nr,1,byteorder='little')
+  p_packet[PACKET_CNUMBER_INDEX] = int.to_bytes(~pkt_nr,1,byteorder='little',signed=True)
+
+  #Filename packet has valid data */
+  p_packet[PACKET_DATA_INDEX , PACKET_DATA_INDEX + size] = p_source[:size]
+ 
+  if ( size  <= packet_size):
+    for i in range(size + PACKET_DATA_INDEX ,packet_size + PACKET_DATA_INDEX):
+        p_packet[i] = 0x1A #EOF (0x1A) or 0x00 */
+
+
+def UpdateCRC16(crc_in, byte):
+  crc = crc_in
+  inbyte = byte | 0x100
+  condition=1
+  while(condition):
+    crc <<= 1
+    inbyte <<= 1
+    if(inbyte & 0x100):
+      crc +=1
+    if(crc & 0x10000):
+      crc ^= 0x1021
+    condition=((inbyte & 0x10000)==0)
+  return (crc & 0xffff)
+
+
+def Cal_CRC16(p_data, size):
+  crc = 0
+  for i in range(size):
+    crc = UpdateCRC16(crc, p_data[i])
+  crc = UpdateCRC16(crc, 0)
+  crc = UpdateCRC16(crc, 0)
+  return crc&0xffff
+
+def CalcChecksum(p_data, size):
+  sum = 0
+  for i in range(size):
+    sum += p_data[i]
+  return (sum & 0xff)
+
+
+
+
+def Ymodem_Transmit(filePath):
+
+  p_file_name=os.path.basename(filePath)
+  p_buf = open(filePath, 'rb').read()
+  file_size=len(p_buf)
+
+  errors = 0
+  ack_recpt = 0
+  size = 0
+  pkt_size=0
+  p_buf_int=0
+  result = COM_OK
+  blk_number = 1
+  a_rx_ctrl=bytearray(2)
+  i=0  
+  temp_crc=0
+
+
+  #Prepare first block - header */
+  PrepareIntialPacket(aPacketData, p_file_name, file_size)
+
+  while((ack_recpt==0 ) and ( result == COM_OK )):
+    #Send Packet */
+    canbus.transmit(aPacketData[PACKET_START_INDEX:], PACKET_SIZE + PACKET_HEADER_SIZE)
+
+    #Send CRC or Check Sum based on CRC16_F */
+    temp_crc = Cal_CRC16(aPacketData[PACKET_DATA_INDEX:], PACKET_SIZE)
+    canbus.transmit([(temp_crc >> 8)],1)
+    canbus.transmit([temp_crc & 0xFF],1)
+
+    #Wait for Ack and 'C' */
+    if (canbus.receive(a_rx_ctrl, 1) == COM_OK):
+      if (a_rx_ctrl[0] == ACK):
+        ack_recpt = 1
+      elif (a_rx_ctrl[0] == CA):
+        if ((canbus.receive(a_rx_ctrl, 1) == COM_OK) and (a_rx_ctrl[0] == CA)):
+          time.sleep( 2 )
+          result = COM_ABORT
+    else:
+      errors+=1
+
+    if (errors >= MAX_ERRORS):
+      result = COM_ERROR
+    
+
+  p_buf_int = 0
+  size = file_size
+
+  #Here 1024 bytes length is used to send the packets */
+  while ((size) and (result == COM_OK )):
+  
+    #Prepare next packet */
+    PreparePacket(p_buf[p_buf_int:], aPacketData, blk_number, size)
+    ack_recpt = 0
+    a_rx_ctrl[0] = 0
+    errors = 0
+
+    #Resend packet if NAK for few times else end of communication */
+    while (( ack_recpt==0 ) and ( result == COM_OK )):
+    
+      #Send next packet */
+      if (size >= PACKET_1K_SIZE):
+      
+        pkt_size = PACKET_1K_SIZE
+      
+      else:
+      
+        pkt_size = PACKET_SIZE
+      
+
+      canbus.transmit(aPacketData[PACKET_START_INDEX:], pkt_size + PACKET_HEADER_SIZE)
+      
+      #Send CRC or Check Sum based on CRC16_F */
+      temp_crc = Cal_CRC16(aPacketData[PACKET_DATA_INDEX:], pkt_size)
+      canbus.transmit(temp_crc >> 8,1)
+      canbus.transmit(temp_crc & 0xFF,1)
+
+      
+      #Wait for Ack */
+      if ((canbus.receive(a_rx_ctrl, 1) == COM_OK) and (a_rx_ctrl[0] == ACK)):
+      
+        ack_recpt = 1
+        if (size > pkt_size):
+          p_buf_int += pkt_size
+          size -= pkt_size
+          if (blk_number == (USER_FLASH_SIZE / PACKET_1K_SIZE)):
+            result = COM_LIMIT #boundary error */
+          else:
+            blk_number+=1
+        else:
+          p_buf_int += pkt_size
+          size = 0
+      else:
+        errors+=1
+      #Resend packet if NAK  for a count of 10 else end of communication */
+      if (errors >= MAX_ERRORS):
+        result = COM_ERROR
+    
+  #Sending End Of Transmission char */
+  ack_recpt = 0
+  a_rx_ctrl[0] = 0x00
+  errors = 0
+  while (( ack_recpt==0 ) and ( result == COM_OK )):
+
+    canbus.transmit(EOT,1)
+
+    #Wait for Ack */
+    if (canbus.receive(a_rx_ctrl, 1) == COM_OK):
+      if (a_rx_ctrl[0] == ACK):
+        ack_recpt = 1
+      elif (a_rx_ctrl[0] == CA):
+        if ((canbus.receive(a_rx_ctrl, 1) == COM_OK) and (a_rx_ctrl[0] == CA)):
+          time.sleep( 2 )
+          result = COM_ABORT
+    else:
+      errors+=1
+    if (errors >=  MAX_ERRORS):
+      result = COM_ERROR
+
+  #Empty packet sent - some terminal emulators need this to close session */
+  if ( result == COM_OK ):
+  
+    #Preparing an empty packet */
+    aPacketData[PACKET_START_INDEX] = SOH
+    aPacketData[PACKET_NUMBER_INDEX] = 0
+    aPacketData[PACKET_CNUMBER_INDEX] = 0xFF
+
+    for i in range(PACKET_DATA_INDEX , PACKET_SIZE + PACKET_DATA_INDEX):
+      aPacketData[i] = 0x00
+
+    #Send Packet */
+    canbus.transmit(aPacketData[PACKET_START_INDEX:], PACKET_SIZE + PACKET_HEADER_SIZE)
+
+    #Send CRC or Check Sum based on CRC16_F */  
+    temp_crc = Cal_CRC16(aPacketData[PACKET_DATA_INDEX:], PACKET_SIZE)
+    canbus.transmit(temp_crc >> 8,1)
+    canbus.transmit(temp_crc & 0xFF,1)
+
+
+    #Wait for Ack and 'C' */
+    if (canbus.receive(a_rx_ctrl, 1) == COM_OK):
+         if (a_rx_ctrl[0] == CA):
+             time.sleep( 2 )
+             result = COM_ABORT
+
+  return result #file transmitted successfully */
+
+
+
+def run():
+  canbus.open(0)
+  canbus.open(1)
+  canbus.start_keyboard()
+  canbus.start_receiving(1)
+
+  binPath="/home/softarm/catkin_ws/src/usbCAN/hydrosoft_IMU_V3.bin"
+  if(canbus.getInput()=='t'):
+    Ymodem_Transmit(binPath)
+
+run()
